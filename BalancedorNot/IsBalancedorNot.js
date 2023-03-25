e417955affc529e0344c2c76619d34db7854c8fb
murali-miriyala <97337635+murali-miriyala@users.noreply.github.com>
2023-03-25 11:51:00 +0530

    Add files via upload

---
+++ BalancedorNot/IsBalancedorNot.js
@@ -0,0 +1,67 @@
+
+
+function isBalancedEnclosure(s) {
+
+
+    let stack = [];
+
+    for (let i = 0; i < s.length; i++) {
+        let x = s[i];
+
+        if (x == '(' || x == '[' || x == '{') {
+
+            stack.push(x);
+            continue;
+        }
+
+        if (stack.length == 0)
+            return false;
+
+        let check;
+        switch (x) {
+            case ')':
+                check = stack.pop();
+                if (check == '{' || check == '[')
+                    return false;
+                break;
+
+            case '}':
+                check = stack.pop();
+                if (check == '(' || check == '[')
+                    return false;
+                break;
+
+            case ']':
+                check = stack.pop();
+                if (check == '(' || check == '{')
+                    return false;
+                break;
+        }
+    }
+
+
+    return (stack.length == 0);
+}
+
+
+let s = "{()[()]}";
+
+
+if (isBalancedEnclosure(s))
+    console.log("True");
+else
+    console.log("False");
+
+// Output : True
+
+
+
+// let s = "[}()]";
+
+
+// if (isBalancedEnclosure(s))
+//     console.log("True");
+// else
+//     console.log("False");
+
+// Ouput : False
